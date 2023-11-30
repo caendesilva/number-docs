535b22425139e338f5e20ee29d5e7519105a1743
Caen De Silva <caen@desilva.se>
2023-11-30 14:42:37 +0100

    Parse readme

--- .github/docs/doc-gen.php
+++ .github/docs/doc-gen.php
@@ -7,11 +7,15 @@ use FriendsOfPhp\Number\Number;
 /** @internal This class is used by the Number package to generate documentation for the package, using reflection and source code evaluation. It is not intended to be used outside of the package. */
 class DocumentationGenerator
 {
+    protected readonly ReadmeData $readmeContents;
+    protected readonly array $readmeData;
+
     protected array $markdownSections = [];
     protected string $markdown;
 
     public function generate(): void
     {
+        $this->loadAndParseReadmeData();
         $this->assembleDocument();
         $this->compileDocument();
     }
@@ -19,6 +23,11 @@ class DocumentationGenerator
     public function getMarkdown(): string
     {
         return $this->markdown;
+    }
+
+    protected function loadAndParseReadmeData(): void
+    {
+        $this->readmeContents = new ReadmeData();
     }
 
     protected function assembleDocument(): void {
@@ -39,6 +48,71 @@ class DocumentationGenerator
     }
 }
 
+/** @internal Data object for the Readme */
+class ReadmeData
+{
+    protected readonly string $contents;
+    protected readonly array $lines;
+    protected readonly array $blocks;
+    protected array $data = [];
+
+    public function __construct()
+    {
+        $this->contents = file_get_contents(__DIR__.'/../../README.md');
+        $this->lines = explode("\n", $this->contents);
+        $this->parseReadme();
+    }
+
+    protected function parseReadme(): void
+    {
+        // Start by generating blocks. Each block starts with a heading, and ends with the next heading or the end of the file.
+        $blocks = [];
+        $currentBlock = null;
+        foreach ($this->lines as $line) {
+            if (str_starts_with($line, '#')) {
+                if ($currentBlock) {
+                    $blocks[] = $currentBlock;
+                }
+                $currentBlock = new MarkdownBlock(
+                    new MarkdownHeading(
+                        trim($line, '# '),
+                        substr_count($line, '#')
+                    ),
+                    ''
+                );
+            } else {
+                $currentBlock->addLine($line);
+            }
+        }
+
+        // Add the last block
+        $blocks[] = $currentBlock;
+
+        // Iterate over blocks to set string identifiers
+        $parsedBlocks = [];
+        foreach ($blocks as $block) {
+            // kebab-case of title
+            $id = strtolower(str_replace(' ', '-', $block->getHeading()->getText()));
+            // If id is already in use, append a number
+            if (isset($parsedBlocks[$id])) {
+                $number = 1;
+                while (isset($parsedBlocks[$id.'-'.$number])) {
+                    $number++;
+                }
+                $id .= '-'.$number;
+            }
+            $parsedBlocks[$id] = $block;
+        }
+
+        $this->blocks = $parsedBlocks;
+    }
+
+    public function __get(string $name)
+    {
+        return $this->data[$name] ?? null;
+    }
+}
+
 /** @internal Represents a Markdown section */
 class MarkdownBlock implements Stringable {
     protected MarkdownHeading $heading;
@@ -53,6 +127,16 @@ class MarkdownBlock implements Stringable {
     public function __toString(): string
     {
         return $this->heading."\n\n".$this->content;
+    }
+
+    public function addLine(string $line): void
+    {
+        $this->content .= $line."\n";
+    }
+
+    public function getHeading(): MarkdownHeading
+    {
+        return $this->heading;
     }
 }
 
@@ -71,6 +155,16 @@ class MarkdownHeading implements Stringable {
     {
         return str_repeat('#', $this->level).' '.$this->text;
     }
+
+    public function getText(): string
+    {
+        return $this->text;
+    }
+
+    public function getLevel(): int
+    {
+        return $this->level;
+    }
 }
 
 
@@ -79,3 +173,10 @@ $generator = new DocumentationGenerator();
 $generator->generate();
 
 echo $generator->getMarkdown();
+
+
+function dd($data)
+{
+    var_dump($data);
+    die;
+}
